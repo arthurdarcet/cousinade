e6194668fc037138e046688de1d5d44263f160a0
Arthur Darcet <arthur.darcet@m4x.org>
2013-01-28 21:57:18 +0100

    Fix wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -13,10 +13,10 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
